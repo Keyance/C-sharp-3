e949ff88e602dc0148cb23322c9398e7a7285f0e
Keyance <klara.konopacova@seznam.cz>
2025-10-05 18:45:26 +0200

    řešení DU2

--- ToDoList/src/ToDoList.WebApi/Program.cs
+++ ToDoList/src/ToDoList.WebApi/Program.cs
@@ -2,5 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/nazdarSvete", () => "Ahoj - tady je pozdrav v češtině.... Hurá!");
 
 app.Run();
