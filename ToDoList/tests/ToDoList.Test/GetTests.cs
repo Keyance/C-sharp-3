16df3181df02a1ea1c129c9b8e49781926ef6c61
Keyance <klara.konopacova@seznam.cz>
2025-10-26 19:27:55 +0100

    úkol 4 (nedokončeno) + hodina 5

--- ToDoList/tests/ToDoList.Test/GetTests.cs
+++ ToDoList/tests/ToDoList.Test/GetTests.cs
@@ -4,6 +4,7 @@ using System.Reflection;
 using ToDoList.Domain.Models;
 using ToDoList.WebApi;
 using Microsoft.AspNetCore.Mvc;
+using ToDoList.Domain.DTOs;
 
 public class GetTests
 {
@@ -41,30 +42,6 @@ public class GetTests
         Assert.Equal(todoItem1.Name, firstToDo.name);
         Assert.Equal(todoItem1.Description, firstToDo.description);
         Assert.Equal(todoItem1.IsCompleted, firstToDo.isCompleted);
-    }
-    [Fact]
-    public void DeleteTest()
-    {
-        ToDoItemsController.items.Clear();
-        // Arrange
-        var controller = new ToDoItemsController();
-        ToDoItemsController.items.Add(new ToDoItem
-        {
-            ToDoItemId = 1,
-            Name = "DeletedItem",
-            Description = "delete",
-            IsCompleted = true
-        });
-
-        //zadefinování controleru a
-
-
-        // Act
-        var result = controller.DeleteById(1);
-
-        // Assert
-        Assert.IsType<NoContentResult>(result);
-        Assert.DoesNotContain(ToDoItemsController.items, i => i.ToDoItemId == 1);
     }
 
 }
