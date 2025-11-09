074419e3a51b2d6f47f4bdbbd5034bc7222faed2
Keyance <klara.konopacova@seznam.cz>
2025-11-09 13:53:42 +0100

    Upraven controller, IRepository a ToDoItemRepository - hodina + úkol 6

--- ToDoList/src/ToDoList.Persistence/Repositories/IRepository.cs
+++ ToDoList/src/ToDoList.Persistence/Repositories/IRepository.cs
@@ -3,5 +3,18 @@ namespace ToDoList.Persistence.Repositories;
 public interface IRepository<T>
     where T : class
 {
+    //CREATE AN ITEM
     public void Create(T item);
+
+    //READ ALL
+    public IEnumerable<T> GetAll();
+
+    //READ BY ID
+    T? GetById(int id); //otazník znamená, že proměná může být i null
+
+    // UPDATE
+    bool Update(int id, T request);
+
+    // DELETE
+    bool Delete(int id);
 }
---
+++ ToDoList/tests/ToDoList.Test/UnitTests/PostTests.cs
@@ -0,0 +1,41 @@
+namespace ToDoList.Test;
+
+using NSubstitute;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Domain.DTOs;
+using ToDoList.WebApi;
+using ToDoList.Persistence;
+using ToDoList.Persistence.Repositories;
+using ToDoList.Domain.Models;
+using ToDoList.Test;
+
+public class PostTests
+{
+    [Fact]
+    public void Post_ValidRequest_ReturnsNewItem()
+    {
+        // Arrange
+        var repositoryMock = Substitute.For<IRepository<ToDoItem>>();
+
+        var controller = new ToDoItemsController(null, repository: null); //toto je zatím prasárna, protože na context už pak nesaháme
+
+        var request = new ToDoItemCreateRequestDto(
+            Name: "Jmeno",
+            Description: "Popis",
+            IsCompleted: false
+        );
+
+        // Act
+        var result = controller.Create(request); //zase zůstává stejné
+        var resultResult = result.Result;
+        var value = result.GetValue();
+
+        // Assert
+        Assert.IsType<CreatedAtActionResult>(resultResult);
+        Assert.NotNull(value);
+
+        Assert.Equal(request.Description, value.description);
+        Assert.Equal(request.IsCompleted, value.isCompleted);
+        Assert.Equal(request.Name, value.name);
+    }
+}
