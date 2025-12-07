cf540ef96d8657230f4378eb95ffc15ae745a8af
Keyance <klara.konopacova@seznam.cz>
2025-12-07 11:39:41 +0100

    dohánění h.10 - vol.1

--- ToDoList/src/ToDoList.Frontend/Clients/ToDoItemsClient.cs
+++ ToDoList/src/ToDoList.Frontend/Clients/ToDoItemsClient.cs
@@ -17,13 +17,34 @@ public class ToDoItemsClient : IToDoItemsClient
         var toDoItemViews = new List<ToDoItemView>();
         var response = await httpClient.GetFromJsonAsync<List<ToDoItemGetResponseDto>>("api/ToDoItems");
 
-        toDoItemViews = response.Select(dto => new ToDoItemView(
-            dto.toDoItemId,
-            dto.name,
-            dto.description,
-            dto.isCompleted
-            )).ToList();
+        toDoItemViews = response.Select(dto => new ToDoItemView()
+        {
+            Id = dto.toDoItemId,
+            Name = dto.name,
+            Description = dto.description,
+            IsCompleted = dto.isCompleted
+        }).ToList();
 
         return toDoItemViews;
+    }
+    public async Task<ToDoItemView?> ReadItemByIdAsync(int itemId)
+    {
+        var response = await httpClient.GetFromJsonAsync<ToDoItemGetResponseDto>($"api/ToDoItems/{itemId}");
+
+        var toDoItem = new ToDoItemView()
+        {
+            Id = response.toDoItemId,
+            Name = response.name,
+            Description = response.description,
+            IsCompleted = response.isCompleted
+        };
+        return toDoItem;
+    }
+
+    public async Task UpdateItemAsync(ToDoItemView item)
+    {
+        // try {}
+        var itemRequest = new ToDoItemUpdateRequestDto(item.Name, item.Description, item.IsCompleted);
+        var response = await httpClient.PutAsJsonAsync($"api/ToDoItems/{item.Id}", itemRequest);
     }
 }
