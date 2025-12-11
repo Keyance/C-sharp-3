e77c7d86eee6b8ae9ec1a113a14fb504e36b3cb6
Keyance <klara.konopacova@seznam.cz>
2025-12-11 20:42:42 +0100

    DTO změněny

--- ToDoList/src/ToDoList.Domain/DTOs/ToDoItemGetResponseDto.cs
+++ ToDoList/src/ToDoList.Domain/DTOs/ToDoItemGetResponseDto.cs
@@ -5,10 +5,10 @@ namespace ToDoList.Domain.DTOs;
 using System.Collections.Generic;
 using ToDoList.Domain.Models;
 
-public record ToDoItemGetResponseDto(int toDoItemId, string name, string description, bool isCompleted)
+public record ToDoItemGetResponseDto(int toDoItemId, string name, string description, bool isCompleted, string? Kategory)
 {
     //public IEnumerable<object>? Id { get; set; }
 
-    public static ToDoItemGetResponseDto FromDomain(ToDoItem item) => new(item.ToDoItemId, item.Name, item.Description, item.IsCompleted);
+    public static ToDoItemGetResponseDto FromDomain(ToDoItem item) => new(item.ToDoItemId, item.Name, item.Description, item.IsCompleted, item.Kategory);
 
 }
